5b7e42129283dcf8f9f887407f35b306d62e8c37
AaveChaine <akchain22@gmail.com>
2022-08-26 20:52:52 +0530

    updated

---
+++ README.md
@@ -0,0 +1,8 @@
+deploy_contract
+
+directions:
+
+npm install
+Add Your Provider ENDPOINT / URL to deploy.js
+Add Your SEED PHRASE to deploy.js
+RUN node deploy.js
--- deploy.js
+++ deploy.js
@@ -2,11 +2,11 @@
 const fs = require('fs');
 const path = require('path');
 const solc = require('solc');
-const Web3 = require('web3');
+const Web3 = require('Web3');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 /** ENTER YOUR INFORMATION HERE! **/
-const mnemonic = '<a94afd8bc5954d429d19f8ba8f2c1240>';     /* YOUR SEED PHRASE ... */
+const mnemonic = '<YOUR SEED PHRASE HERE>';     /* YOUR SEED PHRASE ... */
 const providerOrUrl = '<https://ropsten.infura.io/v3/a94afd8bc5954d429d19f8ba8f2c1240>' /* RINKEBY ENDPOINT */
 
 const provider = new HDWalletProvider({ mnemonic, providerOrUrl });
